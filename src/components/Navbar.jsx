880a43e1c36c65011f5ea65203590a09faaf78b2
Arianny Arias <ariannyariasg@gmail.com>
2021-08-01 23:33:15 -0300

    Agregue el navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+
+
+export default function Navbar() {
+    return(
+        <>
+        <div className = 'navBar' >
+            <div className = 'logo'>
+                <h1><span>be</span>icecream</h1>
+            </div>
+        <div className = 'lista'>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About us</a>
+                </li>
+                <li>
+                    <a href="">Our products</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </div>
+
+        <div className = 'cart'>
+                <i class="fas fa-shopping-cart"></i>
+        </div>
+        </div>
+        </>
+    )
+}
